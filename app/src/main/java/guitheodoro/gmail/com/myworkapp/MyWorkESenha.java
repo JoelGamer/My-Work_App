ca743981a02ff20278159c418511cfbc56ec9ceb
JoelGamer <gui.theodoro2010@gmail.com>
2018-05-12 22:36:02 -0300

    My-Work A-1.1
    Aprofundamento do uso do firebase

--- app/src/main/java/guitheodoro/gmail/com/myworkapp/MyWorkESenha.java
+++ app/src/main/java/guitheodoro/gmail/com/myworkapp/MyWorkESenha.java
@@ -1,24 +1,54 @@
 package guitheodoro.gmail.com.myworkapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+import java.sql.ResultSet;
 
 import static android.support.design.widget.Snackbar.LENGTH_LONG;
 
 public class MyWorkESenha extends AppCompatActivity {
 
-    private int sckbar = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_work_esenha);
+
+        RecuperarSenha();
+    }
+
+    private void RecuperarSenha(){
+        Button btnRec = findViewById(R.id.btnEnviarEmail);
+        final EditText email = findViewById(R.id.txtEmailES);
+
+        btnRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Snackbar.make(findViewById(R.id.MyWorkESenhaLayout), "O Email foi enviado!", LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 
     @Override
@@ -34,7 +64,7 @@ public class MyWorkESenha extends AppCompatActivity {
                 nav_set = new Intent(this, MyWorkSettings.class);
                 break;
             case R.id.nav_autor:
-                SnackbarCreate(this.sckbar);
+                SnackbarCreate();
         }
         if(nav_set != null){
             startActivity(nav_set);
@@ -42,10 +72,12 @@ public class MyWorkESenha extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void SnackbarCreate(int sckbar){
-        if(sckbar == 1) {
-            Snackbar.make(findViewById(R.id.MyWorkESenhaLayout), "Ainda não tem a autoria!", LENGTH_LONG).show();
-        }
-        else{}
+    private void SnackbarCreate(){
+        Snackbar.make(findViewById(R.id.MyWorkESenhaLayout), "Ainda não tem a autoria!", LENGTH_LONG).show();
+    }
+
+    public void Click(View v){
+        Intent reg = new Intent(this, MyWorkLogin.class);
+        startActivity(reg);
     }
 }
--- app/src/main/java/guitheodoro/gmail/com/myworkapp/MyWorkMain.java
+++ app/src/main/java/guitheodoro/gmail/com/myworkapp/MyWorkMain.java
@@ -1,11 +1,7 @@
 package guitheodoro.gmail.com.myworkapp;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,12 +11,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static android.support.design.widget.Snackbar.LENGTH_LONG;
 
 public class MyWorkMain extends AppCompatActivity {
 
-    private int sckbar = 1;
     private ArrayAdapter adapter;
+    private List<String> dataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +38,28 @@ public class MyWorkMain extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.nav_main);
         ListView listView = findViewById(R.id.lstMain);
-
-        adapter = new ArrayAdapter<String>(this, R.layout.my_work_main_list, R.id.Itemname, getResources().getStringArray(R.array.main));
+        
         listView.setAdapter(adapter);
+    }
+
+    private void UpdateList(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference().child("/Empresas");
+        final TextView item = findViewById(R.id.Itemname);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren()){
+                    dataList.add(data.toString());
+                }
+                adapter = new ArrayAdapter<>(MyWorkMain.this, R.layout.my_work_main_list, R.id.Itemname, dataList);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                System.out.println("The read failed: " + error.getCode());
+            }
+        });
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -42,21 +68,18 @@ public class MyWorkMain extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             ListView lstMain = findViewById(R.id.lstMain);
-            BottomNavigationView navigation = findViewById(R.id.nav_menu);
             switch (item.getItemId()) {
                 case R.id.nav_main:
-                    lstMain.setVisibility(View.VISIBLE);
-                    adapter = new ArrayAdapter<String>(MyWorkMain.this, R.layout.my_work_main_list, R.id.Itemname, getResources().getStringArray(R.array.main));
-                    lstMain.setAdapter(adapter);
+                    UpdateList();
                     return true;
                 case R.id.nav_chat:
                     lstMain.setVisibility(View.VISIBLE);
-                    adapter = new ArrayAdapter<String>(MyWorkMain.this, R.layout.my_work_main_list, R.id.Itemname, getResources().getStringArray(R.array.chat));
+                    adapter = new ArrayAdapter<>(MyWorkMain.this, R.layout.my_work_main_list, R.id.Itemname, getResources().getStringArray(R.array.chat));
                     lstMain.setAdapter(adapter);
                     return true;
                 case R.id.nav_notf:
                     lstMain.setVisibility(View.VISIBLE);
-                    adapter = new ArrayAdapter<String>(MyWorkMain.this, R.layout.my_work_main_list, R.id.Itemname, getResources().getStringArray(R.array.notf));
+                    adapter = new ArrayAdapter<>(MyWorkMain.this, R.layout.my_work_main_list, R.id.Itemname, getResources().getStringArray(R.array.notf));
                     lstMain.setAdapter(adapter);
                     return true;
                 case R.id.nav_curr:
@@ -83,7 +106,7 @@ public class MyWorkMain extends AppCompatActivity {
                 nav_set = new Intent(this, MyWorkSettings.class);
                 break;
             case R.id.nav_autor:
-                SnackbarCreate(this.sckbar);
+                SnackbarCreate();
         }
         if(nav_set != null){
             startActivity(nav_set);
@@ -91,10 +114,7 @@ public class MyWorkMain extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void SnackbarCreate(int sckbar){
-        if(sckbar == 1) {
-            Snackbar.make(findViewById(R.id.MyWorkMainLayout), "Ainda não tem a autoria!", LENGTH_LONG).show();
-        }
-        else{}
+    private void SnackbarCreate(){
+        Snackbar.make(findViewById(R.id.MyWorkMainLayout), "Ainda não tem a autoria!", LENGTH_LONG).show();
     }
 }
